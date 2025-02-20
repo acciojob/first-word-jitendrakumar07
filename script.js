3cd7f1e2c587c8d5bdab53005cafa84857dcd0ff
JITENDRA KUMAR <jitendrakumar77800@gmail.com>
2025-02-20 17:17:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function firstWord(s) {
   // your code here
+	 s = s.trimStart();
+  
+
+  const spaceIndex = s.indexOf(" ");
+  
+   return spaceIndex === -1 ? s : s.substring(0, spaceIndex);
+
 }
 
 // Do not change the code below
